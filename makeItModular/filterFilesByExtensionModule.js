ccafc848ce79d4f072a63aed7b6151019ab4cd6a
Ferran Marlet <ferran.marlet@gmail.com>
2015-10-12 02:22:15 +0200

    Make It Modular exercice added.

--- makeItModular/filterFilesByExtensionModule.js
+++ makeItModular/filterFilesByExtensionModule.js
@@ -1,15 +1,46 @@
-module.exports = filterFilesByExtension;
+// This module implements and exports the same function used on "filteredLS"
 
-function filterFilesByExtension(dirPath,fileExtension,function filter(
+var fs = require('fs')
+var path = require('path')
 
-	fs.readdir(dirPath, function (err, list) {
-		  list.forEach(function (file) {
-		  if (path.extname(file) === '.' + fileExtension)
-		      console.log(file)
-		  })
-		}))
-	){
-
-	var fs = require('fs')
-	var path = require('path')
+module.exports = function listFilesFilteredByExtension(directoryPath,selectedFileExtension,callback) {
+	fs.readdir(directoryPath,function listFilesFilteredByExtension(err,list)
+		{
+			if (err) {
+				callback(err, null);
+    		} else {
+				var result = new Array()
+				for (var i = 0; i < list.length; i++) {
+					var indexOfPoint = list[i].lastIndexOf(".")
+					if (indexOfPoint != -1) {
+						var fileExtension = list[i].substring(indexOfPoint+1,list[i].length);
+						if (fileExtension == selectedFileExtension) {
+							result.push(list[i])
+						}
+					}
+				}
+				callback(null,result);
+			}
+		})
 }
+
+/* Solution given by the learnyounode team:
+
+var fs = require('fs')
+var path = require('path')
+
+module.exports = function (dir, filterStr, callback) {
+
+  fs.readdir(dir, function (err, list) {
+	if (err)
+	  return callback(err)
+
+	list = list.filter(function (file) {
+	  return path.extname(file) === '.' + filterStr
+	})
+
+	callback(null, list)
+  })
+}
+
+*/
--- makeItModular/makeItModular.js
+++ makeItModular/makeItModular.js
@@ -1,2 +1,32 @@
+// Same as the filteredLS exercise but encapsulating the filtering function into a module
 
-var filterFilesByExtensionModule = require('./filterFilesByExtensionModule.js')
+// We list all the fies in a directory whose path is passed as an argument.
+// We only list the files of a filetype that are equal to the second passed argument.
+// We do it in an Asyncronous way
+
+var filterFilesByExtensionFunction = require('./filterFilesByExtensionModule.js')
+
+var directoryPath = process.argv[2]
+var selectedFileExtension = process.argv[3]
+
+filterFilesByExtensionFunction(directoryPath,selectedFileExtension,function(err,list){
+  for (i = 0; i < list.length; i++) {
+    console.log(list[i]);
+  }
+})
+
+/* Solution given by the learnyounode team:
+
+var filterFn = require('./solution_filter.js')
+var dir = process.argv[2]
+var filterStr = process.argv[3]
+
+filterFn(dir, filterStr, function (err, list) {
+  if (err)
+    return console.error('There was an error:', err)
+
+  list.forEach(function (file) {
+    console.log(file)
+  })
+})
+*/
